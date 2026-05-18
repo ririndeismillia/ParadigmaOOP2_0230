f80b341af62cae99e493dfa1f29893c0675c0d77
ririndeismillia <ririndeismillia@gmail.com>
2026-05-18 19:05:32 +0700

    class baseClass final public

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -7,21 +7,3 @@ public:
         cout << "Halo saya Function dari base class";
     }
 };
-class baseClass1 {
-public:
-    virtual void perkenalan() {
-        cout << "Halo saya Function dari base class";
-    }
-};
-class derivedClass : public baseClass1 {
-public:
-    void perkenalan() {
-        cout << "Halo saya Function dari derived class";
-    }
-};
-int main() {
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-}
