141628a4bd2bc00dd747770490085002220a1e6a
ririndeismillia <ririndeismillia@gmail.com>
2026-05-18 19:00:42 +0700

    membuat class orang public

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,37 +11,3 @@ public:
     }
 };
 
-class pekerja : virtual public orang {
-public:
-    pekerja(int pUmur) :
-        orang(pUmur)
-    {
-        cout << "pekerja dibuat\n" << endl;
-    }
-};
-
-class pelajar : virtual public orang {
-public:
-    pelajar(int pUmur) :
-        orang(pUmur) 
-    {
-        cout << "pelajar dibuat\n" << endl;
-    }
-};
-class budi : public pekerja, pelajar {
-public:
-
-    budi(int pUmur) :
-        pekerja(pUmur),
-        pelajar(pUmur),
-        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
-    {
-        cout << "Budi dibuat\n" << endl;
-    }
-};
-
-int main() {
-    budi a(12);
-
-    return 0;
-}
