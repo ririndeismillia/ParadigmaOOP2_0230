b04693a615a0814d54aa9831aa1d21311b50e0a5
ririndeismillia <ririndeismillia@gmail.com>
2026-05-18 18:59:57 +0700

    public

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,4 +4,17 @@ using namespace std;
 class AbstraksiKlas {
 private: string x, y;
 
+public:
+   // method untuk mengisin nilai
+   // private member
+   void setXY(string a, string b) {
+      x = a;
+      y = b;
+   }
+   //menampilkan nilai
+   void display() {
+      cout << "x = " << x << endl;
+      cout << "y = " << y << endl;
+    }
 };
+
